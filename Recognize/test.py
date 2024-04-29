31d97e12da3ac15f516a569309d240519bcf5464
jidw <jiyiji2814@gmail.com>
2024-04-29 09:44:08 +0800

    添加绘图功能

--- Recognize/test.py
+++ Recognize/test.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import torchvision
+import matplotlib.pyplot as plt
 
 from models._import import LeNet5, MLP, ResNet, GRU, ViT
 
@@ -11,21 +12,48 @@ test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
 # 定义模型
-net = ResNet()
+net = GRU()
 
 # 加载模型参数
 model_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'save', net._get_name() + '.pth')
 net.load_state_dict(torch.load(model_dir))
 
-# 测试模型
+# 计算模型精度
 net.eval()
 with torch.no_grad():
     correct = 0
     total = 0
-    for images, labels in test_loader:
-        outputs = net(images)
+    for image, label in test_loader:
+        outputs = net(image)
         _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        total += label.size(0)
+        correct += (predicted == label).sum().item()
 
-    print('Test Accuracy: {:.2f}%'.format(100 * correct / total))
+    accuracy = 100 * correct / total
+    print('Test Accuracy: {:.2f}%'.format(accuracy))
+
+# 加载测试集
+examples = enumerate(test_loader)
+_, (example_images, example_targets) = next(examples)
+
+# 显示图片
+fig = plt.figure()
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.tight_layout()
+    plt.imshow(example_images[i][0], cmap='gray', interpolation='none')
+
+    example_outputs = net(example_images)
+    _, example_predicts = torch.max(example_outputs.data, dim=1)
+    
+    plt.title("Ground Truth: {} \n Predict: {}".format(example_targets[i], example_predicts[i]))
+
+    '''移除刻度'''
+    plt.xticks([])
+    plt.yticks([])
+
+# 显示精确度
+accuracy_text = "Accuracy: {:.2f}%".format(accuracy)
+plt.figtext(0.5, 0.01, accuracy_text, ha='center', fontsize=12)
+
+plt.show()
